7f3d4733e5d01bb777ecec76399e2e3d85cc0213
Marc Berchtold <me@echolot.io>
2024-10-30 18:06:29 +0100

    Create ssh connection and exec cmd on request

--- constants.ts
+++ constants.ts
@@ -1,3 +1,5 @@
 export const PORT = process.env.PORT || 3000;
 export const MODEM_HOST = process.env.MODEM_HOST || '192.168.2.1';
+export const MODEM_USERNAME = process.env.MODEM_USERNAME || 'admin';
+export const MODEM_PASSWORD = process.env.MODEM_PASSWORD || 'admin';
 export const METRIC_NAME_PREFIX = process.env.METRIC_NAME_PREFIX || 'vigor';
--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
+import {createServer} from 'http';
+import {NodeSSH} from 'node-ssh';
 import {Registry, Gauge} from 'prom-client';
 import {getName} from './utils';
-import {MODEM_HOST, PORT} from './constants';
-import {createServer} from 'http';
+import {MODEM_HOST, MODEM_PASSWORD, MODEM_USERNAME, PORT} from './constants';
+
+const ssh = new NodeSSH();
 
 const registry = new Registry();
 registry.setDefaultLabels({
@@ -21,6 +24,23 @@ const server = createServer(async (req, res) => {
   }
 
   if (req.url === '/metrics') {
+    await ssh.connect({
+      host: MODEM_HOST,
+      username: MODEM_USERNAME,
+      password: MODEM_PASSWORD,
+      algorithms: {
+        kex: {append: 'diffie-hellman-group1-sha1', prepend: [], remove: []},
+        serverHostKey: {append: 'ssh-dss', prepend: [], remove: []},
+      }
+    });
+    const shell = await ssh.requestShell();
+    shell.on('data', (data: Buffer) => {
+      console.log(data.toString());
+    });
+    shell.write('vdsl status\n');
+    shell.close();
+    ssh.dispose();
+
     res.setHeader('Content-Type', registry.contentType);
     res.end(await registry.metrics());
   }
